bf68fcbe05f792cb40d72ccbdc4827e3528c88ef
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2024-04-04 17:21:44 +0000

    mkcxxxr with inline reference functions
    ref: https://stackoverflow.com/a/78275509/23626926

--- mkcxxxr.py
+++ mkcxxxr.py
@@ -1,9 +1,15 @@
+#! /usr/bin/env python
+
 def mkcxxxr(n, bw):
     if bw == 1 and (n == 0 or n == 1):
-        return f"#define c{'ad'[n]}r(x) ((x)->c{'ad'[n]}r)"
+        return f"inline object*& c{'ad'[n]}r(object* x) {{ return x->c{'ad'[n]}r; }}"
     binstr = bin(n)[2:].rjust(bw, '0').replace('1', 'd').replace('0', 'a')
-    return f"#define c{binstr}r(x) c{binstr[0]}r(c{binstr[1:]}r(x))"
+    print("c" + binstr + "r")
+    return f"inline object*& c{binstr}r(object* x) {{ return c{binstr[0]}r(c{binstr[1:]}r(x)); }}"
 
-for bw in range(1, 7):
-    for n in range(2**bw):
-        print(mkcxxxr(n, bw))
+
+with open("cxxxr.h", "w") as f:
+    print('/* AUTO-GENERATED FILE */\n#include "tinobsy.hpp"', file=f)
+    for bw in range(1, 7):
+        for n in range(2**bw):
+            print(mkcxxxr(n, bw), file=f)
